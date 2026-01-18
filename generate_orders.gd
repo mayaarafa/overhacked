c36d5ccc8e5d1e0ff4724dc7e30b39e8f7f73856
J-Young <jyoung222@my.bcit.ca>
2026-01-17 18:55:47 -0800

    add signal to update text on dictionary change using setter

--- generate_orders.gd
+++ generate_orders.gd
@@ -1,20 +1,46 @@
 extends Node3D
 
+# FSM for interactions
+# generate order
+# loop 1
+# player comes close with item
+# check if right item
+# if so display check mark
+# player places item
+# sets equivalent dictionary to true
+# check if all dictionary entries are true
+# if not - loop 1
+# if yes:
+# start here*
+# generate random order_cooldown in range
+# set on_cooldown to true
+# Remove order - done
+# display no text on project_order
+# wait for cooldown timer to finish
+# #generate new order
+
 const ITEMSCODES = ['filament', 'source', 'matcha', 'coffee', 'tea', '3D', 'library', 'code block'];;
+var RNG = RandomNumberGenerator.new();
+signal dictionary_changed
+var order_cooldown;
+var on_cooldown;
 var project_size;
-var order = [];
+var order = {};
+@onready var cooldown: Timer = get_node("order_generation_cooldown");
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	project_size = randi() % 5;
-	for i in range(project_size):
-		var rand_item = get_rand_item_not_in_array(order);
-		order.append(rand_item);
+	begin_cooldown(true);
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if (on_cooldown && cooldown.is_stopped()):
+		on_cooldown = false;
+		
+	
 
-func get_rand_item_not_in_array(pre_existing: Array = []):
+func get_rand_item_not_in_array(pre_existing: Dictionary = {}):
 	# If all items are already used, return null
 	if pre_existing.size() >= ITEMSCODES.size():
 		return null
@@ -22,3 +48,28 @@ func get_rand_item_not_in_array(pre_existing: Array = []):
 	while pre_existing.has(item):
 		item = ITEMSCODES.pick_random()
 	return item
+	
+func generate_new_order():
+	order = {}
+	project_size = randi() % 5;
+	for i in range(project_size):
+		var rand_item = get_rand_item_not_in_array(order);
+		order[rand_item] = false;
+	print(order);
+
+func begin_cooldown(flag: bool = false):
+	var random_int;
+	set_dictionary({});
+	if (true):
+		random_int = RNG.randi_range(0, 21);
+	else:
+		random_int = RNG.randi_range(15, 46);
+	cooldown.wait_time = random_int;
+	cooldown.start();
+	on_cooldown = true;
+
+func set_dictionary(new_dict):
+	order = new_dict;
+	var text_project_order = get_node("text_project_order");
+	text_project_order.update_dict(order);
+	
--- start.gd
+++ start.gd
@@ -1,6 +1,6 @@
-extends Label3D
-# update label with new orders, or display 
-# check mark for already delivered components
+extends Node3D
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,6 +9,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func update_dict(dict):
-	text = JSON.stringify(dict);
